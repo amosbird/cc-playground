8b3395846fe743f94926c0ffd56f44201e47bba5
Amos Bird <amosbird@gmail.com>
2018-10-17 16:14:38 +0800

    workaround ccls issue

--- templates/snippet.cpp
+++ templates/snippet.cpp
@@ -2,13 +2,11 @@
 // Execute the snippet with Ctrl-Return
 // Remove the snippet completely with its dir and all files M-x `cc-playground-rm`
 
-#include <ccutils/strings.hpp>
 #include <iostream>
 
 using namespace std;
-using namespace ccutils;
 
-#ifndef NOMAIN
-int main(int argc, char *argv[]) {
+int main() {
+
+    return 0;
 }
-#endif
